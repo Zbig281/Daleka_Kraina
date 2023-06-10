6f5a48fd156bf37aa68f78511507ec427ad10eab
Zbig Brodaty <silver281@wp.pl~>
2023-06-10 23:59:36 +0200

    small_fix!

--- Tradepost/config.cs
+++ Tradepost/config.cs
@@ -1,19 +1,19 @@
 // FeudalTools Server-ID
 $FTTP::ServerID = 8497;
 // Customization
-<<<<<<< HEAD
+
 $FTTP::TradeMessageText = "Naciśnij CTRL+T aby rozpoczat handel w tym miejscu tradepost.";
 $FTTP::TradeMessageTime = 9999;
 $FTTP::Keybind = "ctrl t";
 // Text for missing client mod
 $FTTP::ModMissingCenterText = "Tradepost! Nie posiadasz moda. Jezeli wiesz ze go masz zrob Restart gry.";
 $FTTP::ModMissingSystemText = "Ten server uzywa Tradepost Mod. Aby handlować tutaj, potrzebujesz Tradepost Client Mod. Możesz zainstalować go przez YoLauncher. Albo cale info znajduje sie na kanale Linki";
-=======
+
 $FTTP::TradeMessageText = "Press CTRL+T to start trading at this tradepost.";
 $FTTP::TradeMessageTime = 9999;
 $FTTP::Keybind = "ctrl t";
 // Text for missing client mod
 $FTTP::ModMissingCenterText = "Tradepost client-mod missing. Check your system chat.";
 $FTTP::ModMissingSystemText = "This server is using the Tradepost Server Mod. In order to trade here, you need the Tradepost Client Mod. You can install it through YoLauncher.";
->>>>>>> dee8314ed5761ce663f2b31a79420702548329de
+
 
